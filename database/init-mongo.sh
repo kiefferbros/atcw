dcaebb15904ae16a001357b4c6f49a40a18da932
Jonathan Kieffer <jdkieffer@gmail.com>
2023-11-16 23:16:01 -0600

    fix monog init script

--- database/init-mongo.sh
+++ database/init-mongo.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+mongosh -- "$MONGO_INITDB_DATABASE" <<EOF
     var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
     var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
     var admin = db.getSiblingDB('admin');
